c3b5d5a4a81a5b8f2604e26fac1847b6c241bf0b
Gabriel Ion <gabriel.ion2005@gmail.com>
2026-03-23 20:22:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from "./Home.jsx";
+
+function App() {
+
+  return(
+        <div className="App">
+          <h2>Welcome to our front end</h2>
+          <Home/>
+        </div>
+    );
+}
+
+export default App
